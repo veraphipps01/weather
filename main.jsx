a73952e050b60670fc28e2649b90ada3fedd0635
Vera Phipps <85977298+veraphipps01@users.noreply.github.com>
2024-08-25 15:12:19 -0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -20,7 +20,7 @@ class App extends React.Component {
       let safelat = position.coords.latitude ? position.coords.latitude : 0;
       let safelon = position.coords.longitude ? position.coords.longitude : 0;
       fetch(
-        ""https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"" +
+        "https://api.wunderground.com/api/d6fadca18738e4ec/geolookup/conditions/forecast/q/" +
           safelat +
           "&lon=" +
           safelon
