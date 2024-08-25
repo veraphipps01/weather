afd078a9e3e692fc58c21a5db16b4243f2397acc
Vera Phipps <85977298+veraphipps01@users.noreply.github.com>
2024-08-25 15:07:11 -0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -20,7 +20,7 @@ class App extends React.Component {
       let safelat = position.coords.latitude ? position.coords.latitude : 0;
       let safelon = position.coords.longitude ? position.coords.longitude : 0;
       fetch(
-        "https://fcc-weather-api.glitch.me/api/current?lat=" +
+        ""https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"" +
           safelat +
           "&lon=" +
           safelon
